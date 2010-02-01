4af3248bbab94ba2284b532c280d955b0f4e4d0a
ashwin.h.kalbhor <ashwin.h.kalbhor@539e1900-f385-11de-82c3-25da7dca7fdf>
2010-02-01 07:34:37 +0000

    Enhancement to ImagePanel1 class..

--- CCProject/src/control/ImagePanel1.java
+++ CCProject/src/control/ImagePanel1.java
@@ -8,30 +8,18 @@ import javax.swing.JPanel;
 public class ImagePanel1 extends JPanel{
 	//private String path;
 	//private ImageHandler IH;
-	public ImagePanel1(){
+	public ImagePanel1(String path){
 		setBorder(BorderFactory.createLineBorder(new Color(0,0,0)));
 		setLayout(null);
-		//this.path = path;
-		
-		//path = new String("data/Images/1.jpg");
-	}
-	
-	public void loadImagePanel(String path){
-		
 		ImageHandler IH = new ImageHandler(path);
-		IH.setBounds(0, 0, 200, 200);
+		this.add(IH);
+		IH.setSize(550, 400);
 		IH.init();
-		IH.start();
-		//IH.run();
 		IH.setup();
 		IH.draw();
 		
-		
-		//IH.stop();
-		//IH.destroy();
-		//IH.exit();
-		//add(IH);
 	}
+	
 	
 
 }
